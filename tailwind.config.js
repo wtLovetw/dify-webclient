85cbe7264642dc709ace6d8e3c6394c20ee9d9bd
kimna <573034707@qq.com>
2026-03-09 16:33:15 +0800

    修改config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    typography: require('./typography'),
+    // typography: require('./typography'),
     extend: {
       colors: {
         gray: {
